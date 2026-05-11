3cd9ef6b380a4a5aed1d0bbcf975ae506596cece
Lourdes Santillan <santillanlourdes011@gmail.com>
2026-05-11 11:42:48 -0300

    modificaciones varias

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { createClient as createSupabaseClient } from './utils/supabase/middleware';
 
 const isProtectedRoute = createRouteMatcher([
   '/productos(.*)',
@@ -13,8 +14,13 @@ const isProtectedRoute = createRouteMatcher([
   '/api/preferencias(.*)',
 ]);
 
-export default clerkMiddleware((auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
+  // Refresh Supabase session
+  const supabaseResponse = createSupabaseClient(req);
+
   if (isProtectedRoute(req)) auth().protect();
+
+  return supabaseResponse;
 });
 
 export const config = {
